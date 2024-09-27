adb6f7a1f50b139485c58eed273ad5fb0e7bc89a
Pankaj Sharma <99128016+ipankajsharma01@users.noreply.github.com>
2024-09-27 20:43:21 +0530

    Update ptj.js

--- ptj.js
+++ ptj.js
@@ -155,7 +155,7 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+  themeButton.classList.contains(iconTheme) ? "uil-sun" : "uil-light";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
